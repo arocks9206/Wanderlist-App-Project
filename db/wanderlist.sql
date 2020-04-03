7ce7726ea5ef7434873f61c534d6fd6f5066c8be
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-04-03 20:10:39 +0100

    sql_runner and db set-up

---
+++ db/wanderlist.sql
@@ -0,0 +1,26 @@
+DROP TABLE user_profile;
+DROP TABLE bucket_list;
+DROP TABLE destinations;
+DROP TABLE been_there;
+
+CREATE TABLE user_profile (
+  id SERIAL primary key,
+  full_name VARCHAR(255),
+  current_location(255)
+);
+
+CREATE TABLE destinations (
+  id SERIAL primary key,
+  continent VARCHAR(255),
+  country VARCHAR(255),
+  cities VARCHAR(255),
+  sites VARCHAR(255)
+);
+
+CREATE TABLE bucket_list (
+  id SERIAL primary key,
+  continent VARCHAR(255),
+  country VARCHAR(255),
+  cities VARCHAR(255),
+  sites VARCHAR(255)
+);
